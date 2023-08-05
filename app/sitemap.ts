237dfb19e66a86341b24d2656ae1ee68ece44826
sulaiman-coder <51925710+sulaiman-coder@users.noreply.github.com>
2023-08-06 00:31:24 +0600

    Update sitemap.ts
    Signed-off-by: sulaiman-coder <51925710+sulaiman-coder@users.noreply.github.com>

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,11 +3,11 @@ import { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "https://demo.useliftoff.com",
+      url: "https://bdcheap.com",
       lastModified: new Date(),
     },
     {
-      url: "https://demo.useliftoff.com/demo",
+      url: "https://bdcheap.com",
       lastModified: new Date(),
     },
   ];
